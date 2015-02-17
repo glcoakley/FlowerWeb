b22df9961d832a8a94e3d3031bd32e24d9c6b9de
glcoakley <glc@cbcc.cc>
2015-02-16 19:21:58 -0500

    FlowerWeb Start

---
+++ FlowerWeb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlowerWeb
+//
+//  Created by Gary Coakley on 2/16/15.
+//  Copyright (c) 2015 Gary Coakley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
