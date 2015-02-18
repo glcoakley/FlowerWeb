1d723c80d520296f4d6ed95f1d12fca029946c7c
glcoakley <glc@cbcc.cc>
2015-02-17 19:53:12 -0500

    After adding getFlower code

--- FlowerWeb/ViewController.m
+++ FlowerWeb/ViewController.m
@@ -15,6 +15,7 @@
 
 - (IBAction)getFlower:(UIButton *)sender;
 - (IBAction)toggleFlowerDetail:(UISwitch *)sender;
+- (IBAction)xxx:(id)sender;
 
 @end
 
@@ -30,9 +31,32 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)getFlower:(UIButton *)sender {
+- (IBAction)getFlower:(id )sender
+{
+    NSURL *imageURL;
+    NSURL *detailURL;
+    NSString    *imageURLString;
+    NSString    *detailURLString;
+    NSString    *color;
+    int         sessionID;
+    
+    color = [self.selColorChoice titleForSegmentAtIndex:self.selColorChoice.selectedSegmentIndex];
+    sessionID = random()%50000;
+    imageURLString = [NSString stringWithFormat:@"http://www.floraphotographs.com/showrandomios.php?color=%@&session=%d",color, sessionID];
+    detailURLString = [NSString stringWithFormat:@"http://www.floraphotographs.com/detailios.php?session=%d", sessionID];
+    imageURL=[NSURL URLWithString:imageURLString];
+    detailURL=[NSURL URLWithString:detailURLString];
+    [self.webvFlowerView loadRequest:[NSURLRequest requestWithURL:imageURL]];
+    [self.webvFlowerDetailView loadRequest:[NSURLRequest requestWithURL:detailURL]];
 }
 
-- (IBAction)toggleFlowerDetail:(UISwitch *)sender {
+- (IBAction)toggleFlowerDetail:(UISwitch *)sender
+{
+    self.webvFlowerDetailView.hidden = [sender isOn];
+}
+
+- (IBAction)xxx:(id)sender
+{
+    [self getFlower:sender];
 }
 @end
